2624900ff0a70347213e73a4c7266364aead57ad
K-Mo2 <karimelsisi27@gmail.com>
2025-01-23 22:50:58 +0200

    Update env varaibles

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const apiClient = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+});
 
 apiClient.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem("accessToken");
